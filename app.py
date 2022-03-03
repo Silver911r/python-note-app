0980985b638d255399230fe581d794acf22d2ec4
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-03-03 18:18:45 -0500

    add routes

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 from flask_pymongo import PyMongo
 
 import config
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = f"mongodb+srv://silver911r:{config.mongo_pass}@cluster0.ggyrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = f"mongodb+srv://{config.username}:{config.mongo_pass}@cluster0.ggyrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def home():
     return "<p>The home page</p>"
+
+
+@app.route('/add-note', methods=['GET', 'POST'])
+def addNote():
+    if(request.method == "GET"):
+        return "<p>Add note page</p>"
+    elif(request.method == "POST"):
+        # logic for adding a note
+        pass
+
+
+@app.route('/edit-note', methods=["GET", "POST"])
+def editNote():
+    if(request.method == "GET"):
+        return "<p>Edit note page</p>"
+    elif(request.method == "POST"):
+        # logic for editing a note
+        pass
+
+
+@app.route('/delete-note', methods=["POST"])
+def deleteNote():
+    # logic here
+    pass
 
 
 if __name__ == "__main__":
